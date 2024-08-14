3242a257391bf6e7136d74887b0bbbd4b1cd6fbc
IndiraPriyadharshini-63 <indirapriyadharshini.mary@gmail.com>
2024-08-14 21:26:30 +0530

    Added Login, Register and addpin components
    register and login user, added pin in the backend and retrieve pins to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,20 @@ import { Room, Star } from "@mui/icons-material";
 import "./App.css";
 import axios from "axios";
 import { format } from "timeago.js";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
-  const currentUser = "aaa";
+  const myStorage = window.localStorage;
+  const [currentUser, setCurrentUser] = useState(myStorage.getItem("user"));
   const [pins, setPins] = useState([]);
   const [currentPlaceId, setCurrentPlaceId] = useState(null);
   const [newPlace, setNewPlace] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [rating, setRating] = useState(0);
+  const [showRegister, setShowRegister] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
   const [viewport, setViewport] = useState({
     longitude: -122.4,
     latitude: 37.8,
@@ -29,33 +37,73 @@ function App() {
   useEffect(() => {
     getPins();
   }, []);
-  const handleMarkerClick = (id) => {
+
+  const handleMarkerClick = (id, lat, long) => {
     setCurrentPlaceId(id);
+    setViewport({ ...viewport, latitude: lat, longitude: long });
+    console.log(viewport);
   };
 
   const handleAddClick = (e) => {
-    console.log(e);
+    setNewPlace({
+      lat: e.lngLat.lat,
+      lng: e.lngLat.lng,
+    });
+    // console.log(newPlace.lat)
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPin = {
+      username: currentUser,
+      title,
+      desc,
+      rating,
+      lat: newPlace.lat,
+      long: newPlace.lng,
+    };
+
+    try {
+      const res = await axios.post("/pins", newPin);
+      setPins([...pins, res.data]);
+      setNewPlace(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleLogout = () => {
+    myStorage.removeItem("user");
+    setCurrentUser(null);
   };
 
   return (
     <div className="App">
       <Map
+        // {...viewport}
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
         initialViewState={viewport}
         style={{ width: "100vw", height: "100vh" }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
-        onDblClick={(e)=>handleAddClick(e)}
+        onDblClick={handleAddClick}
+        transitionDuration="200"
       >
         {pins.map((p) => (
           <>
-            <Marker longitude={p.long} latitude={p.lat} anchor="bottom">
+            <Marker
+              longitude={p.long}
+              latitude={p.lat}
+              offsetLeft={-viewport.zoom * 7}
+              offsetRight={-viewport.zoom * 3.5}
+            >
               <Room
+                key={p.lat}
                 style={{
                   fontSize: viewport.zoom * 7,
                   color: p.username === currentUser ? "tomato" : "slateblue",
                   cursor: "pointer",
                 }}
-                onClick={() => handleMarkerClick(p._id)}
+                onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
               />
             </Marker>
             {p._id === currentPlaceId && (
@@ -74,11 +122,7 @@ function App() {
                   <p className="desc">{p.desc}</p>
                   <label>Rating</label>
                   <div className="stars">
-                    <Star className="star" />
-                    <Star className="star" />
-                    <Star className="star" />
-                    <Star className="star" />
-                    <Star className="star" />
+                    {Array(p.rating).fill(<Star className="star" />)}
                   </div>
                   <label>Information</label>
                   <span className="username">
@@ -91,17 +135,67 @@ function App() {
             )}
           </>
         ))}
-        {/* <Popup 
-           latitude={}
-           longitude={}
-             closeButton={true}
-             closeOnClick={false}
-             anchor="left"
-             onClose={()=>setCurrentPlaceId(null)}
-           
-        >
-          hello
-        </Popup> */}
+        {newPlace && (
+          <Popup
+            latitude={newPlace.lat}
+            longitude={newPlace.lng}
+            closeButton={true}
+            closeOnClick={false}
+            anchor="left"
+            onClose={() => setNewPlace(null)}
+          >
+            <div>
+              <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input
+                  placeholder="enter a title"
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Review</label>
+                <textarea
+                  placeholder="Say about this place"
+                  onChange={(e) => setDesc(e.target.value)}
+                />
+                <label>Rating</label>
+                <select onChange={(e) => setRating(e.target.value)}>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+                <button type="submit" className="submitButton">
+                  Add Pin
+                </button>
+              </form>
+            </div>
+          </Popup>
+        )}
+        {currentUser ? (
+          <button className="button logout" onClick={handleLogout}>
+            Log out
+          </button>
+        ) : (
+          <div className="buttons">
+            <button className="button login" onClick={() => setShowLogin(true)}>
+              Login
+            </button>
+            <button
+              className="button register"
+              onClick={() => setShowRegister(true)}
+            >
+              Register{" "}
+            </button>
+          </div>
+        )}
+        {showRegister && <Register setShowRegister={setShowRegister} />}
+        {showLogin && (
+          <Login
+            setShowLogin={setShowLogin}
+            myStorage={myStorage}
+            setCurrentUser={setCurrentUser}
+          />
+        )}
       </Map>
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  // eslint-disable-next-line
   <React.StrictMode>
     <App />
   </React.StrictMode>
